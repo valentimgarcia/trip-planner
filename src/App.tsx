bb7c8d1fc4f34e13ab5759948c2cd63fabfc9845
Valentim Garcia <valentim.garcia@nau-21.com>
2024-07-11 00:27:50 +0100

    Main component development

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">
+          Invite your friends and plan your next trip!
+        </p>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="bg-transparent text-lg placeholder-zinc-400"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="When?"
+              className="bg-transparent text-lg placeholder-zinc-400"
+            />
+          </div>
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continue
+            <ArrowRight className="size-5 text-lime-950" />
+          </button>
+        </div>
+
+        <p className="text-zinc-500 text-sm">
+          By planning your trip through plann.er you automatically agree <br />
+          to our{" "}
+          <a href="#" className="text-zinc-300 underline">
+            terms of use
+          </a>{" "}
+          and{" "}
+          <a href="#" className="text-zinc-300 underline">
+            privacy policies
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
